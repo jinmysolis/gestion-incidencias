0e4878552b9d2a623c5805e7fc564e873e66ba1e
jinmy <solisjinmy@gmail.com>
2017-04-01 11:16:39 -0430

    primer commi-vistas

---
+++ resources/views/includes/menu.blade.php
@@ -0,0 +1,18 @@
+<div class="panel panel-primary">
+    <div class="panel-heading">Menu</div>
+    <div class="panel-body">
+        <div class="list-group">
+            <a href="#" class="list-group-item ">
+              Cras justo odio
+            </a>
+            <a href="#" class="list-group-item">Dapibus ac facilisis in
+            </a>
+            <a href="#" class="list-group-item">Morbi leo risus
+            </a>
+          </div>
+    </div>
+</div>
+
+
+
+
